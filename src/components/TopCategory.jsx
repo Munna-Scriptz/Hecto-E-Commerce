8315b672a466478e5630ea59dc9ceaf4a001cb9f
Munna-Scripts <munnascriptz@gmail.com>
2025-08-19 21:11:21 +0600

    Top Category Api added

--- src/components/TopCategory.jsx
+++ src/components/TopCategory.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import CommonHead from './common/CommonHead'
+import SingleCategory from './common/SingleCategory'
+import Slider from 'react-slick';
+import axios from 'axios';
 
 const TopCategory = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 2500,
+  };
+  // ---------------Api--------------
+  // ------Hooks 
+  const [product , setProduct] = useState([])
+  // ------Axios 
+  useEffect(() => {
+      axios.get('https://api.escuelajs.co/api/v1/products/?categoryId=3')
+      .then((res)=>(setProduct(res.data)))
+      .catch((err)=>(console.log(err)))
+  }, []);
   return (
     <>
-    <section id='TopCategory'>
+    <section id='TopCategory' className='mt-[80px]'>
         <div className="container">
             <div className="TopCategory-Row">
-
+              <CommonHead text={'Top Categories'}/>
+              {/* -----------Category Item-------------  */}
+              <div className='mt-[42px]'>
+                    <div className="slider-Category-container">
+                        <Slider {...settings}>
+                          {
+                            product.map((item , i )=>(
+                              <SingleCategory proImg={item.images[0]} proName={item.title} ProDesc={item.description} proPrice={item.price}/>
+                            ))
+                          }
+                        </Slider>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
--- src/components/common/SingleCategory.jsx
+++ src/components/common/SingleCategory.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ProductItem from '../../assets/images/ProductImage.png'
 import { Link } from 'react-router'
-const SingleCategory = () => {
+const SingleCategory = ({proImg , proName , proPrice}) => {
   return (
     <>
     <div className='w-[270px] flex flex-col items-center relative group duration-[.3s] cursor-pointer'>
         <div className=' absolute bg-BlueText top-[300px] opacity-0 group-hover:top-[220px] group-hover:opacity-100 duration-[.3s]'>
             <Link className='bg-[#08D15F] h-[32px] px-[18px] font-josefin text-white rounded-[2px] flex items-center hover:scale-[1.07] duration-[.3s] justify-center' to={'/'}>View Shop</Link>
         </div>
-        <img src={ProductItem} alt="Product Image" />
-        <h2 className='text-BlueText text-[20px] font-josefin mt-[25px]'>Mini LCW Chair</h2>
-        <p className='text-BlueText text-base font-josefin mt-[13px]'>$56.00</p>
+        <img src={proImg} alt="Product Image" />
+        <h2 className='text-BlueText text-[20px] font-josefin mt-[25px] line-clamp-1'>{proName}</h2>
+        <p className='text-BlueText text-base font-josefin mt-[13px]'>${proPrice}</p>
     </div>
     </>
   )
