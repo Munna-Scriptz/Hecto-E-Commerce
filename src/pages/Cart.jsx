c265a04613cf89acf2fe5c9e062ad1edac5b9153
Munna-Scripts <munnascriptz@gmail.com>
2025-09-29 22:43:08 +0600

    Ux added when theres no cart porduct added

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,7 +4,8 @@ import CartLeftSide from '../components/cart/CartLeftSide'
 import CartRightSide from '../components/cart/CartRightSide'
 import axios from 'axios'
 import { useSelector } from 'react-redux'
-
+import noProImg from '../../src/assets/images/noProFound.png'
+import { Link } from 'react-router'
 const Cart = () => {
   const [product , setProduct] = useState([])
   const Ids = useSelector(state => state.MyRedux.value)
@@ -12,6 +13,7 @@ const Cart = () => {
   const filteredPro = product.filter((item)=>{
     return Ids.includes(item.id)
   })
+  console.log(filteredPro)
   // ---------------Api--------------
     useEffect(() => {
       axios.get(`https://dummyjson.com/products/`)
@@ -24,12 +26,21 @@ const Cart = () => {
         <BreadCrumb pageName={'Shopping Cart'} to={'cart'} />
         <section id='Cart-Page' className='mt-[112px]'>
             <div className="container">
+              {
+                filteredPro.length == 0?
+                <div className='flex flex-col items-center justify-center'>
+                  <img className='w-[600px]' src={noProImg} alt="No Product Added yet..." />
+                  <h2 className='text-BlueText text-[32px] font-josefin mt-4'>No products added yet...</h2>
+                  <Link className='bg-brand text-white inline-block py-[12px] px-[32px] rounded-[4px] font-josefin hover:bg-subText duration-300' to={'/shop'}>Back to Shop</Link>
+                </div>
+                :
                 <div id="Cart-Page-Row" className='flex items-start justify-between'>
                     {/* --------------------Left side  -------------------- */}
                     <CartLeftSide products={filteredPro}/>
                     {/* --------------------Right side --------------------  */}
                     <CartRightSide products={filteredPro} />
                 </div>
+              }
             </div>
         </section>
     </>
